5eb6097b4a58700147c18e014fdbc962fb0a71d9
Tobias Haar <tobias.haar@gmail.com>
2015-01-30 15:46:54 +0000

    added code to detect octal file/dir permissions

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("modstat: Error: Expected file.")
+		os.Exit(1)
+	}
+
+	fileName := os.Args[1]
+	file, err := os.Open(fileName)
+	if err != nil {
+		fail(err)
+	}
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+		fail(err)
+	}
+
+	mode := fileInfo.Mode()
+	if mode.IsDir() {
+		mode = mode ^ os.ModeDir
+	}
+
+	fmt.Printf("0%o\t%v\n", mode, path.Clean(fileName))
+}
+
+func fail(err error) {
+	fmt.Println(err)
+	os.Exit(1)
+}
